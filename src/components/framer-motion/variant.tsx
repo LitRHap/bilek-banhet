a4d15b191939a76dd398669e208023ecd1088489
SirGhazian <ghazianunp99@gmail.com>
2024-07-03 06:21:58 +0700

    first commit

---
+++ src/components/framer-motion/variant.tsx
@@ -0,0 +1,9 @@
+export const variants1 = {
+  open: { opacity: 1, display: "inline" },
+  close: { opacity: 0, display: "none" },
+};
+
+export const variants2 = {
+  open: { opacity: 1, display: "inline" },
+  close: { opacity: 0, display: "none" },
+};
---
+++ src/data/listData.tsx
@@ -0,0 +1,19 @@
+const listData = [
+  { nama: "sigma", rarity: 99 },
+  { nama: "skibidi sigma", rarity: 97 },
+  { nama: "mewing king", rarity: 96 },
+  { nama: "w rizzer", rarity: 96 },
+  { nama: "gyatt level 100", rarity: 96 },
+  { nama: "ishowspeed", rarity: 90 },
+  { nama: "ohio sigma", rarity: 90 },
+  { nama: "bintang sigma", rarity: 90 },
+  { nama: "laba-laba jawa", rarity: 84 },
+  { nama: "jawir ireng", rarity: 84 },
+  { nama: "mio mirza", rarity: 84 },
+  { nama: "kai cenat", rarity: 84 },
+  { nama: "fanum tax", rarity: 84 },
+  { nama: "L rizzler", rarity: 60 },
+  { nama: "skibidi toilet", rarity: 60 },
+];
+
+export default listData;
