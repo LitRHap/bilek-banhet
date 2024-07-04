808f12c657f03af6b745a740211be1f0da8809e4
SirGhazian <ghazianunp99@gmail.com>
2024-07-04 15:02:49 +0700

    fix ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         content="width=device-width, initial-scale=1.0, user-scalable=no"
       ></meta>
       <body className={inter.className}>
-        <AppBgImg />
+        {/* <AppBgImg /> */}
         {children}
       </body>
     </html>
