628b9e6921f13b23330f746f2ffdd2387eb6572a
SirGhazian <ghazianunp99@gmail.com>
2024-07-04 14:59:59 +0700

    fixed ui section 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CekBrainrot from "../components/checkGenerator";
 export default function Home() {
   return (
     <>
-      <main className="min-h-[100vh] w-full">
+      <main className="max-h-[100vh] w-full">
         <CekBrainrot />
       </main>
     </>
