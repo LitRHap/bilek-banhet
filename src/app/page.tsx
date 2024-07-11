73413aa328473e589ad75e46da860d6584805b05
SirGhazian <ghazianunp99@gmail.com>
2024-07-11 21:13:35 +0700

    add preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import CheckBrainrot from "../components/checkGenerator";
 import dynamic from "next/dynamic";
 import Loading from "./loading";
+import PreLoader from "@/components/preloader";
 
 const CheckBrainrot = dynamic(
   () => import("../components/checkGenerator"),
@@ -13,6 +14,7 @@ const CheckBrainrot = dynamic(
 export default function Home() {
   return (
     <>
+      <PreLoader />
       <main className="max-h-[100vh] w-full">
         <CheckBrainrot />
       </main>
---
+++ src/components/preloader.tsx
@@ -0,0 +1,36 @@
+"use client";
+import style from "@/components/css/loading.module.css";
+import { motion } from "framer-motion";
+import React, {
+  useEffect,
+  useState,
+} from "react";
+
+export default function PreLoader() {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      <motion.main
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        transition={{ delay: 0.4, duration: 0.5 }}
+        className="absolute flex items-center min-w-[100vw] min-h-[100vh] bg-white z-40 pointer-events-none"
+      >
+        <section className={style.dotscontainer}>
+          <div className={style.dot}></div>
+          <div className={style.dot}></div>
+          <div className={style.dot}></div>
+          <div className={style.dot}></div>
+          <div className={style.dot}></div>
+        </section>
+      </motion.main>
+    </>
+  );
+}
