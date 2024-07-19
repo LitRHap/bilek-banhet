0491c44db967a96d4310e8d67f359901387d01dd
SirGhazian <ghazianunp99@gmail.com>
2024-07-19 18:50:44 +0700

    added data

--- src/data/listData.tsx
+++ src/data/listData.tsx
@@ -1,13 +1,20 @@
 const listData = [
   // A Tier
   { name: "sigma", aura: "+9999" },
+  { name: "gigachad", aura: "+9999" },
+  { name: "gedagedigedao", aura: "+9999" },
   { name: "looksmaxing", aura: "+9999" },
   { name: "skibidi sigma", aura: "+9999" },
   { name: "gyatt level 100", aura: "+9999" },
+  { name: "penaldo", aura: "+9999" },
   { name: "mewing king", aura: "+9000" },
   { name: "w rizzler", aura: "+8500" },
+  { name: "amogus", aura: "+7500" },
+  { name: "pessi", aura: "+7000" },
 
   // B Tier
+  { name: "sus", aura: "+600" },
+  { name: "grimace shake", aura: "+550" },
   { name: "bocil windah", aura: "+500" },
   { name: "alpha", aura: "+250" },
   { name: "bintang skibidi", aura: "+150" },
@@ -20,6 +27,8 @@ const listData = [
 
   // C Tier
   { name: "L rizzler", aura: "-9999" },
+  { name: "caseoh", aura: "-9999" },
+  { name: "anitamaxwin", aura: "-9999" },
   { name: "skibidi toilet", aura: "-9999" },
   { name: "beta", aura: "-9999" },
   { name: "delulu", aura: "-9999" },
