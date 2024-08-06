d8548b20858c6eee8f4febc0f78b5d1e9a371c00
SirGhazian <ghazianunp99@gmail.com>
2024-08-07 00:42:56 +0700

    fixed preloader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import style from "@/components/css/loading.module.css";
 export default function Loading() {
   return (
     <>
-      <main className="flex items-center min-h-[100vh]">
+      <main className="absolute flex items-center w-screen h-screen">
         <section className={style.dotscontainer}>
           <div className={style.dot}></div>
           <div className={style.dot}></div>
--- src/components/preloader.tsx
+++ src/components/preloader.tsx
@@ -1,10 +1,7 @@
 "use client";
 import style from "@/components/css/loading.module.css";
 import { motion } from "framer-motion";
-import React, {
-  useEffect,
-  useState,
-} from "react";
+import React, { useEffect, useState } from "react";
 
 export default function PreLoader() {
   const [show, setShow] = useState(true);
@@ -21,7 +18,7 @@ export default function PreLoader() {
         initial={{ opacity: 1 }}
         animate={{ opacity: 0 }}
         transition={{ delay: 0.4, duration: 0.5 }}
-        className="absolute flex items-center min-w-[100vw] min-h-[100vh] bg-white z-40 pointer-events-none"
+        className="absolute flex items-center w-screen h-screen bg-white z-40 pointer-events-none"
       >
         <section className={style.dotscontainer}>
           <div className={style.dot}></div>
